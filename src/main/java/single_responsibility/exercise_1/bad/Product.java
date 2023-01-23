555a7b5d00f2b888083d49d3171cfc298ea22ff0
elvira.segarraruiz <elvira.segarraruiz@thoughtworks.com>
2023-01-23 12:08:55 +0100

    Refactor

--- src/main/java/single_responsibility/exercise_1/bad/Product.java
+++ src/main/java/single_responsibility/exercise_1/bad/Product.java
@@ -1,36 +1,24 @@
 package single_responsibility.exercise_1.bad;
 
 public class Product {
-    private final String name;
+
     private final double price;
     private final double VAT_GERMANY = 0.19;
     private final double VAT_SPAIN = 0.21;
     private final double VAT_UK = 0.2;
-
     private final double VAT_FRANCE = 0.2;
 
-    public Product(String name, double price) {
-        this.name = name;
+    public Product(double price) {
         this.price = price;
     }
 
     public double calculatePriceWithTax(Country country) {
         return switch (country) {
-            case GERMANY -> this.price * VAT_GERMANY;
-            case SPAIN -> this.price * VAT_SPAIN;
-            case FRANCE -> this.price * VAT_FRANCE;
-            case UK ->  this.price * VAT_UK;
+            case GERMANY -> this.price + this.price * VAT_GERMANY;
+            case SPAIN -> this.price + this.price * VAT_SPAIN;
+            case FRANCE -> this.price + this.price * VAT_FRANCE;
+            case UK -> this.price + this.price * VAT_UK;
         };
     }
 
-    public double calculateTotalPriceWithTaxForOrder(int quantity, Country country) {
-        return quantity * calculatePriceWithTax(country);
-    }
-
-    public enum Country {
-        GERMANY,
-        SPAIN,
-        FRANCE,
-        UK
-    }
 }
--- src/test/java/single_responsibility/exercise_1/good/ProductTest.java
+++ src/test/java/single_responsibility/exercise_1/good/ProductTest.java
@@ -1,6 +1,7 @@
 package single_responsibility.exercise_1.good;
 
 import org.junit.jupiter.api.Test;
+import single_responsibility.exercise_1.bad.Country;
 import single_responsibility.exercise_1.bad.Product;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,26 +10,52 @@ class ProductTest {
 
     @Test
     public void calculateFrancePrice10WithTax(){
-        Product product = new Product("testProduct", 10.0);
+        Product product = new Product(10.0);
 
-        double result = product.calculatePriceWithTax(Product.Country.FRANCE);
-        double expected = 2.0;
-
+        double result = product.calculatePriceWithTax(Country.FRANCE);
+        double expected = 12.0;
 
         assertEquals(expected, result);
-
     }
 
     @Test
     public void calculateFrancePrice20WithTax(){
-        Product product = new Product("testProduct", 20.0);
+        Product product = new Product( 20.0);
 
-        double result = product.calculatePriceWithTax(Product.Country.FRANCE);
-        double expected = 4.0;
-
+        double result = product.calculatePriceWithTax(Country.FRANCE);
+        double expected = 24.0;
 
         assertEquals(expected, result);
+    }
 
+    @Test
+    public void calculateSpainPrice10WithTax(){
+        Product product = new Product(10.0);
+
+        double result = product.calculatePriceWithTax(Country.SPAIN);
+        double expected = 12.1;
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void calculateGermanyPrice10WithTax(){
+        Product product = new Product(10.0);
+
+        double result = product.calculatePriceWithTax(Country.GERMANY);
+        double expected = 11.9;
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void calculateUKPrice10WithTax(){
+        Product product = new Product(10.0);
+
+        double result = product.calculatePriceWithTax(Country.UK);
+        double expected = 12.0;
+
+        assertEquals(expected, result);
     }
 
 }
